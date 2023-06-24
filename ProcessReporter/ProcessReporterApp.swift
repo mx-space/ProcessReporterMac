49d9aacf6acf10ba03d54f4ffc23eb326ed4a736
Innei <i@innei.in>
2023-06-24 10:35:45 +0800

    Initial Commit

---
+++ ProcessReporter/ProcessReporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProcessReporterApp.swift
+//  ProcessReporter
+//
+//  Created by Innei on 2023/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ProcessReporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
